8227b3f61bfe792ed1195ab067de10372955e276
Prashant Singh <prashantsingh.aiengineer@gmail.com>
2025-04-02 18:49:51 +0530

    configfile done

---
+++ app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/hello")
+def read_hello():
+    return {"message": "Hello, FastAPI!"}
+
+
+
+
---
+++ src/langgraph/ui/ui_configfile.py
@@ -0,0 +1,16 @@
+## to read the ui_configfile.ini
+from configparser import ConfigParser
+
+class config:
+    def __init__(self,config_file=".\src\langgraph\ui\ui_configfile.ini"):
+        self.config = ConfigParser()
+        self.config.read(config_file)
+
+    def get_llm_option(self):
+        return self.config["DEFAULT"].get("LLM_OPTIONS").split(",")
+    def get_usercase_option(self):
+        return self.config["DEFAULT"].get("USECASE_OPTION").split(",")
+    def get_groq_model_options(self):
+        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(",")
+    def get_page_title(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE")
